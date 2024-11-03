74a73e25417fcf2dfed2c645f171bcaea9701fa8
Jessy Avalos Barrales <jessyavalos16@outlook.es>
2024-11-02 22:57:14 -0600

    updated ./hook/index

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as useDebounce } from './useDebounce/useDebounce';
+export { default as useDocumentTitle } from './useDocumentTitle/useDocumentTitle';
 export { default as useFetch } from './useFetch/useFetch';
